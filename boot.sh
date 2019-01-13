4567a7c19f5d1940344009eb09e87ccd2a30dad3
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-13 01:34:24 -0500

    edit boot.sh

--- boot.sh
+++ boot.sh
@@ -2,6 +2,12 @@
 
 /isso/bin/gunicorn -b 0.0.0.0:8080 -w 3 --preload isso.run &
 
+if [ ! -e /var/lib/config/isso.cfg ]
+then
+    echo Create config.
+    touch /var/lib/config/isso.config
+fi
+
 while true; do
     while inotifywait -q -e modify -e move_self /var/lib/config/isso.cfg >/dev/null; do
         echo "Isso Config Changed."
