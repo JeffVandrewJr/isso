c5a065d1459564d79096f420f5b3b26976b35464
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-20 19:03:04 -0500

    boot.sh

--- boot.sh
+++ boot.sh
@@ -17,10 +17,8 @@ then
     touch /var/lib/config/isso.config
 fi
 
-while true; do
-    while inotifywait -q -e modify -e move_self /var/lib/config/isso.cfg >/dev/null; do
-        echo "Isso Config Changed."
-        pkill gunicorn
-        /isso/bin/gunicorn -b 0.0.0.0:8080 -w 3 --preload isso.run &
-    done
+while inotifywait -q -e modify -e move_self /var/lib/config/isso.cfg >/dev/null; do
+    echo "Isso Config Changed."
+    pkill gunicorn
+    /isso/bin/gunicorn -b 0.0.0.0:8080 -w 3 --preload isso.run &
 done
