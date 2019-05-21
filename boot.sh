89264c7998f934b97ff1b29d0f8b95e717309ede
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-05-21 17:35:11 -0400

    logging

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/isso/bin/gunicorn -b 0.0.0.0:8080 -w 3 --preload isso.run &
+/isso/bin/gunicorn -b 0.0.0.0:8080 -w 3 --preload isso.run --access-logfile=- --error-logfile=- &
 
 if [ ! -e /var/lib/config/isso.cfg ]
 then
@@ -12,6 +12,6 @@ while true; do
     while inotifywait -q -e modify -e move_self /var/lib/config/isso.cfg >/dev/null; do
         echo "Isso Config Changed."
         pkill gunicorn
-        /isso/bin/gunicorn -b 0.0.0.0:8080 -w 3 --preload isso.run &
+	/isso/bin/gunicorn -b 0.0.0.0:8080 -w 3 --preload isso.run --access-logfile=- --error-logfile=- &
     done
 done
